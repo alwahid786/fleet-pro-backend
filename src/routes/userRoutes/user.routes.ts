6c9cb9284232683137efcb5f08272a1ff207d240
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-06-14 18:02:49 +0500

    feat:add adming apis for update delete and get user

--- src/routes/userRoutes/user.routes.ts
+++ src/routes/userRoutes/user.routes.ts
@@ -1,5 +1,6 @@
 import {
     forgetPassword,
+    getMyProfile,
     getNewAccessToken,
     login,
     logout,
@@ -13,20 +14,23 @@ export const userRoutes = (app: any) => {
     // register user
     app.post("/api/user/register", register);
 
-    // verify registration
-    app.get("/api/user/verify", verifyRegistration);
-
-    // verify registration
-    app.put("/api/user/forget-password", forgetPassword);
-
-    // verify registration
-    app.post("/api/user/reset-password", resetPassword);
-
     // login user
     app.post("/api/user/login", login);
 
     // logout user
     app.get("/api/user/logout", auth, logout);
+
+    // get my profile
+    app.get("/api/user/profile", auth, getMyProfile);
+
+    // verify registration
+    app.get("/api/user/verify", verifyRegistration);
+
+    // forget password
+    app.put("/api/user/forget-password", forgetPassword);
+
+    // reset password
+    app.post("/api/user/reset-password", resetPassword);
 
     // get new access token
     app.get("/api/user/access-token", getNewAccessToken);
