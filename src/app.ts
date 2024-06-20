60c27c9df2cf0ac5291580dcade779e7513ec098
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-06-20 20:27:09 +0500

    ad cor

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,16 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
+import morgan from "morgan";
+import path from "node:path";
+import { __dirName } from "./constants/costants.js";
 import { Errorhandler } from "./middlewares/errorHandler.js";
 import { allApiRoutes } from "./routes/index.routes.js";
-import cookieParser from "cookie-parser";
-import morgan from "morgan";
-import { __dirName } from "./constants/costants.js";
-import path from "node:path";
-import cors from "cors";
-import { config } from "./config/config.js";
-
-export const corsOptions = {
-    origin: [config.getEnv("FRONTEND_URL")],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-};
 
 export const app = express();
 
 // middleware
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
