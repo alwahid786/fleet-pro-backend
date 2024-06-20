98dbf1d37dfc75714d43fb408384daa3689b99e8
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-06-20 14:40:46 +0500

    add some changes in auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,29 +4,51 @@ import jwt from "jsonwebtoken";
 import { config } from "../config/config.js";
 import { TryCatch } from "../utils/tryCatch.js";
 import { User } from "../models/userModel/user.model.js";
+import { JWTService } from "../services/jwtToken.js";
+import { accessTokenOptions, refreshTokenOptions } from "../constants/costants.js";
 
 declare module "express-serve-static-core" {
     interface Request {
         user?: { _id: string; role: string };
     }
 }
+
 export const auth = TryCatch(async (req: Request, res: Response, next: NextFunction) => {
-    const accessToken = req.cookies?.accessToken;
-    if (!accessToken) return next(createHttpError(401, "Unauthorized user please login"));
-    let verifyToken: any;
     try {
-        verifyToken = jwt.verify(accessToken, config.getEnv("ACCESS_TOKEN_SECRET")!);
-    } catch (err) {
-        return next(createHttpError(401, "Unauthorized user please login"));
-    }
-    if (verifyToken) {
-        const user = await User.findById(verifyToken._id).select(["_id", "role"]);
-        if (!user) return next(createHttpError(401, "Unauthorized user please login"));
-        // Set user information in req.user
-        req.user = { _id: String(user._id), role: user?.role };
+        const accessToken = req.cookies?.accessToken;
+        let verifyToken: any;
+        let receivedUser: any;
+        if (accessToken) {
+            verifyToken = jwt.verify(accessToken, config.getEnv("ACCESS_TOKEN_SECRET")!);
+            const user = await User.findById(verifyToken._id).select(["_id", "role"]);
+            if (!user) return next(createHttpError(401, "Unauthorized user please login"));
+            receivedUser = { _id: String(user._id), role: user?.role };
+        } else {
+            const refreshToken = req.cookies?.refreshToken;
+            if (!refreshToken) return next(createHttpError(401, "Unauthorized user please login"));
+            verifyToken = await JWTService().verifyRefreshToken(refreshToken);
+            if (verifyToken) {
+                const user = await User.findById(verifyToken._id).select(["_id", "role"]);
+                if (!user) return next(createHttpError(401, "Unauthorized user please login"));
+                // create new access and refresh token
+                const [newAccessToken, newRefreshToken] = await Promise.all([
+                    JWTService().accessToken(String(user._id)),
+                    JWTService().refreshToken(String(user._id)),
+                ]);
+                // remove old Refresh Token and save new refresh token
+                await Promise.all([
+                    JWTService().removeRefreshToken(String(refreshToken)),
+                    JWTService().storeRefreshToken(String(newRefreshToken)),
+                ]);
+                res.cookie("accessToken", newAccessToken, accessTokenOptions);
+                res.cookie("refreshToken", newRefreshToken, refreshTokenOptions);
+                receivedUser = { _id: String(user._id), role: user?.role };
+            }
+        }
+        req.user = receivedUser;
         next();
-    } else {
-        return next(createHttpError(401, "Unauthorized user please login"));
+    } catch (error) {
+        next(createHttpError(401, "Unauthorized user please login"));
     }
 });
 
