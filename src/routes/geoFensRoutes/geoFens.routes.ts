71552f236b83c9201e62eeed31368e2f5bca217c
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-07-10 12:18:10 +0500

    add

--- src/routes/geoFensRoutes/geoFens.routes.ts
+++ src/routes/geoFensRoutes/geoFens.routes.ts
@@ -9,14 +9,18 @@ import {
 import { auth } from "../../middlewares/auth.js";
 
 export const geoFneceRoutes = (app: any) => {
+    // create geofence
     app.post("/api/geofence/create", auth, createGeoFence);
 
+    // get edit delete geofence
     app.route("/api/geofence/single/:geoFenceId")
         .get(auth, getSingleGeoFence)
         .put(auth, updateSingleGeoFences)
         .delete(auth, deleteSingleGeoFence);
 
+    // add truck and area geofence
     app.put("/api/geofence/add-truck/:geoFenceId", auth, addTruckAndArea);
 
+    // get all geofences
     app.get("/api/geofence/all", auth, getAllGeoFences);
 };
