2c7597ce512329879b161297f2f311eedaebacfa
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-07-23 09:55:35 +0500

    add disable stripe parser

--- src/routes/subscriptionRoutes/subscription.routes.ts
+++ src/routes/subscriptionRoutes/subscription.routes.ts
@@ -6,7 +6,6 @@ import {
 import { auth } from "../../middlewares/auth.js";
 
 export const subscriptionRoutes = (app: any) => {
-    app.use(bodyParser.raw({ type: "application/json" }));
     app.post("/api/subscription/create-session", auth, createStripeSession);
-    app.post("/api/subscription/webhook", addNewSubscription);
+    app.post("/api/subscription/webhook", bodyParser.raw({ type: "application/json" }), addNewSubscription);
 };
