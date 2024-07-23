bd21c06d6a1346874afdc61873fc883a3446ba50
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-07-23 10:48:31 +0500

    add body parser again

--- src/routes/subscriptionRoutes/subscription.routes.ts
+++ src/routes/subscriptionRoutes/subscription.routes.ts
@@ -7,5 +7,5 @@ import { auth } from "../../middlewares/auth.js";
 
 export const subscriptionRoutes = (app: any) => {
     app.post("/api/subscription/create-session", auth, createStripeSession);
-    app.post("/api/subscription/webhook", addNewSubscription);
+    app.post("/api/subscription/webhook", bodyParser.raw({ type: "application/json" }), addNewSubscription);
 };
